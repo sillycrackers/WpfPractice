dfddd6ee700b7fd99949a7486d90d802e98e63d7
sillycrackers <westervelderik@gmail.com>
2021-12-13 22:53:55 -0500

    getting started

---
+++ WpfPractice/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPractice.ViewModels
+{
+    public class MainViewModel
+    {
+        public PersonViewModel person { get; set; }
+        public MainViewModel()
+        {
+            person = new PersonViewModel("Erik");
+        }
+    }
+}
