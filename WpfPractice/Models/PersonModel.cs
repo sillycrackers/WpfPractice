ae3e61bcabaf9505f53e5399afb1040cfdb17070
sillycrackers <westervelderik@gmail.com>
2021-12-14 11:12:43 -0500

    learning

---
+++ WpfPractice/Models/PersonModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfPractice.Models
+{
+    public class PersonModel : ObservableObject
+    {
+        public string Name
+        {
+            get
+            {
+                if (String.IsNullOrWhiteSpace(_name))
+                {
+                    return "No Name";
+                }
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        private string _name;
+
+        public PersonModel()
+        {
+
+        }
+        public PersonModel(string name)
+        {
+            this.Name = name;
+        }
+
+    }
+}
---
+++ WpfPractice/Views/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfPractice.Models;
+
+namespace WpfPractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<PersonModel> people { get; set; }
+        public PersonModel CurrentSelectedPerson { get; set; }
+        public MainWindow()
+        {
+            people = new ObservableCollection<PersonModel>();
+            CurrentSelectedPerson = new PersonModel();
+
+            this.DataContext = this;
+
+            InitializeComponent();
+
+            people.Add(new PersonModel("Joe"));
+            people.Add(new PersonModel("Sandra"));
+            people.Add(new PersonModel("Louis"));
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            people.Add(new PersonModel(textbox1.Text));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            people.Remove((PersonModel)listbox1.SelectedItem);  
+        }
+
+        private void listbox1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
